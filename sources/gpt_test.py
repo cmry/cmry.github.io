3cefe2d6b96bcff110aaeab56319beac23d971d9
Chris Emmery <cmry@protonmail.com>
2022-10-27 10:22:12 +0200

    update script

--- sources/gpt_test.py
+++ sources/gpt_test.py
@@ -10,15 +10,14 @@ prompt_text = "A random"
 
 encoded_prompt = tokenizer(prefix + ' ' + prompt_text,
                            add_special_tokens=False)
-print("\nIDs with <prefix, space, prompt, manual tokens>:\n", encoded_prompt.input_ids)
+print("\nIDs with <prefix, space, prompt, manual special>:\n", encoded_prompt.input_ids)
 
 encoded_prompt = tokenizer(prefix + prompt_text, add_special_tokens=False)
-print("\nIDs with <prefix, nospace, prompt, manual tokens>:\n", encoded_prompt.input_ids)
+print("\nIDs with <prefix, nospace, prompt, manual special>:\n", encoded_prompt.input_ids)
 out_1 = model(tokenizer.encode(prefix + prompt_text, add_special_tokens=False, return_tensors="pt"))
-print("Logits with <prefix, nospace, prompt, manual tokens>:\n", out_1.logits)
+print("Logits with <prefix, nospace, prompt, manual special>:\n", out_1.logits)
 
 encoded_prompt = tokenizer(prompt_text)
-
 print(encoded_prompt.input_ids)
 print("\nIDs with <prompt>:\n", encoded_prompt.input_ids)
 out_2 = model(tokenizer.encode(prompt_text, return_tensors="pt"))
